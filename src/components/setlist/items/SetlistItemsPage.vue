e8accb3a0a5e46af4cf96a260cc959d04319f677
Flavio Santos <flavio.santos@engdb.com.br>
2026-03-09 11:01:39 -0300

    Filter and query

--- src/components/setlist/items/SetlistItemsPage.vue
+++ src/components/setlist/items/SetlistItemsPage.vue
@@ -73,6 +73,12 @@ function onUpdateSetlistItem(value: SetlistItem) {
           >
             Title
           </button>
+          <button
+            @click="setSort('status')"
+            class="px-3 py-1.5 text-sm rounded-md border border-gray-300 hover:bg-gray-100"
+          >
+            Title
+          </button>
         </div>
 
         <select v-model="statusFilter" class="px-3 py-2 text-sm rounded-md border border-gray-300">
