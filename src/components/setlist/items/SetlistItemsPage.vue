a8524e6e8cc2e4dfc116ab71b68aab656803b118
Flavio Santos <flavio.santos@engdb.com.br>
2026-03-15 16:34:42 -0300

    Filter and query

--- src/components/setlist/items/SetlistItemsPage.vue
+++ src/components/setlist/items/SetlistItemsPage.vue
@@ -96,6 +96,12 @@ function onUpdateSetlistItem(value: SetlistItem) {
           >
             Status
           </button>
+          <button
+            @click="setSort('lastRehearsedAt')"
+            class="px-3 py-1.5 text-sm rounded-md border border-gray-300 hover:bg-gray-100"
+          >
+            Ensaio
+          </button>
         </div>
 
         <select v-model="statusFilter" class="px-3 py-2 text-sm rounded-md border border-gray-300">
